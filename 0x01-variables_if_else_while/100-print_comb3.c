e2888efc9e0dcb2eed44d08b64b283600f5026de
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-09 06:15:44 -0700

    me

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible different combinations of three digit
+ * Numbers must be separated by ,, followed by a space
+ * The three digits must be different
+ *
+ * Return: return 0. thank you
+ */
+int main(void)
+{
+	int digit0, digit1, digit2
+
+		for (digit0 = 0; digit0 < 8; digit0++)
+		{
+			for (digit1 = digit0 + 1; digit1 < 9; digit1++)
+			{
+				for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+				{
+					putchar((digit0 % 10) + '0');
+					putchar((digit1 % 10) + '0');
+					putchar((digit2 % 10) + '0');
+
+			if (digit0 == 7 && digit1 == 8 && digit2 ++ 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+			}
+				}
+		putchar('\n');
+
+		return (0)
+}
