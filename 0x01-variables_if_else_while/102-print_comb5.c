ac015aef8b09c0429a4b95ee38dfa246a82c061a
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-09 07:40:54 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			putchar(' ');
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-		if (num0 == 98 & num1 == 99)
+		if (num0 == 98 && num1 == 99)
 			continue;
 		putchar(',');
 		putchar(' ');
