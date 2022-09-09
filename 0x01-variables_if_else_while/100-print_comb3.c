d56bb04a705d34c4a826d10bb11244f679093d71
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-09 07:07:42 -0700

    tired

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,10 +17,10 @@ int main(void)
 
 			putchar((digit1 % 10) + '0');
 
-		if (digit0 == 8 && digit1 == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
+	if (digit0 == 8 && digit1 == 9)
+		continue;
+	putchar(',');
+	putchar(' ');
 
 		}
 	}
