964d8eb60ee58296f8d66d6fd8c3ed9d8f24115a
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-13 10:09:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,32 +8,33 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			k = j * 1;
-		}
+			k = i * j;
 
-			if (j == 0)
+			if ((k / 10) == 0)
 			{
-				_putchar(k + '0');
-			}
-			
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j != 0)
 				_putchar(' ');
 				_putchar(k + '0');
-			}
-			else if (k >= 10)
-			{
+				if (j == 9)
+					continue;
 				_putchar(',');
 				_putchar(' ');
+			}
+			else
+			{
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
 			}
-	}
+		}
+
 		_putchar('\n');
+	}
 }
