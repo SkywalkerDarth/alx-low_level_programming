7a3871ed35e6871f7eba0eb01aa2e5f0f99c2c53
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-14 04:11:05 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,14 @@ int main(void)
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
 	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
+
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
 		if (i == 49)
-			printf("%ld\n", fibonacci[i]);
+			printf("%ld\n ", fibonacci[i]);
 		else
-			printf("%ld ", fibonacci[i]);
+			printf("%ld, ", fibonacci[i]);
 	}
 	return (0);
 }
