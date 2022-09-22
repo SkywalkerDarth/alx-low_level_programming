4b55e8d3e88effb05765e85fd2a2a3ed6d967134
Uchechukwu Daniel <danteuche@gmail.com>
2022-09-22 01:26:54 -0700

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,13 +8,14 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int a = 0;
+	int b = 0;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
+	while (dest[a++])
+	{
+		b++;
+	}
+	for (a = 0; src[a] && a < n; a++)
+		dest[b++] = src[a];
 	return (dest);
 }
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * infinite_add - Write a function that adds two numbers.
+ * @n1: strings to be added
+ * @n2: second number to be added
+ * @r: result of addition
+ * @size_r: size of buffer
+ * Return: r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, size_r--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+
+		*(r + size_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	if 
+}
