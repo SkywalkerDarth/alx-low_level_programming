80ca3cfc37bd71794bf95d2780cedb1f975d627d
Uchechukwu Daniel <danteuche@gmail.com>
2022-10-13 05:17:01 -0700

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,14 +12,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i = 0;
+	unsigned int i;
 	unsigned int sum = 0;
-
-	if (n == 0)
-		return (0);
+	
 	va_start(ap, n);
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
 	}
