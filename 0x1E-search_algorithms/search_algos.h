2c14d899f0c3d91dca457fa1948eca6875b2d115
SkywalkerDarth <danteuche@gmail.com>
2023-04-12 09:04:25 -0700

    jump search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /**
  * struct listint_s - singly linked list
@@ -20,5 +21,5 @@ typedef struct listint_s
 /* prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif
