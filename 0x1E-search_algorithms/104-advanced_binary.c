7206179f247faf26bbc0009894655895707c91fb
SkywalkerDarth <danteuche@gmail.com>
2023-04-13 03:30:52 -0700

    exponential search

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,12 @@
+#include "search_algos.h"
+
+/**
+ * advanced_binary - search mtd
+ * @array: array to search
+ * @size: Array size
+ * @value: Array Value
+ * Return: index value integer
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+
